b9377e96ac5e8c95399262d531dd84bd1fc5a6e4
Sowmya <sowmivenkat8449@gmail.com>
2023-12-14 10:00:14 +0530

    Day2

---
+++ Day-2/myapp/src/App.js
@@ -0,0 +1,14 @@
+import './Assets/Css/style.css'
+function App(){
+    return(
+        <div>
+           <form class="container">
+        <h3>LOGIN PAGE</h3>
+        <input type="text" class="it" name="Username" id="" placeholder="username"/>
+        <input type="password" class="it"name="password" id="" placeholder="password"/>
+        <input type="button"class="bt" value="SUBMIT"/>
+    </form>
+        </div>
+    )
+}
+export default App
---
+++ Day-2/myapp/src/Components/Nav.js
@@ -0,0 +1,14 @@
+import Nav from './Components/Nav'
+function Nav() {
+
+    return(
+        
+        <nav class="navigation">
+            <ul>
+                <li><a href="C:\Users\sowmi\OneDrive\Desktop\T2\react day1.html">REGISTERATION </a></li>
+                <li><a href="C:\Users\sowmi\OneDrive\Desktop\T2\login.html">LOGIN</a></li>
+            </ul>
+    </nav>
+        
+    )
+}
